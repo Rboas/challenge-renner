afc9bd8a6ba4d6ee3b9fe919526c4dfeb9811932
Renato Moreira <renatomvboas@gmail.com>
2022-03-31 16:13:22 -0300

    component header

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { MdShoppingCart, MdPerson } from 'react-icons/md';
+import styles from '../styles/Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.navBar}>
+      <div className={styles.content}>
+        <Image src="/logo_red3.webp" alt="Logo" width={176} height={36} />
+        <div className={styles.userContent}>
+            <span>Entre ou cadastre-se</span>
+            <MdShoppingCart size={22} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
